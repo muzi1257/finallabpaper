d35b3c00c5a7d28421061ac6cad07446f04b83c6
muzi1257 <79784651+muzi1257@users.noreply.github.com>
2021-06-25 17:55:29 +0500

    Create connection.js

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://muzi123:muzammil123@cluster0.ip9h9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected To MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error while connecting to Database");
+    console.log(error.message);
+  });
